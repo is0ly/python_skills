282538889d515bfaa7e5cf85aafe76cfaf3125e9
is0ly <iljaosipov@gmail.com>
2021-01-05 01:03:49 +0300

    insert digit to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from operations.functions import multiply_by_two
 from operations.constants import PI, RADIUS
 
-print(multiply_by_two(99, 4, 67, 456, 55, PI, RADIUS))
+print(multiply_by_two(22, 99, 4, 67, 456, 55, PI, RADIUS))
 
