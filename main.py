4534f919eac5feedabb8c8f290c8c9833c5aac13
is0ly <iljaosipov@gmail.com>
2021-01-05 01:19:39 +0300

    added function 'sum_two_digit'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from operations.functions import multiply_by_two
+from operations.functions import multiply_by_two, sum_two_digit
 from operations.constants import PI, RADIUS
 
 print(multiply_by_two(22, 99, 4, 67, 456, 55, PI, RADIUS))
+print(sum_two_digit(4, 74))
 
--- operations/functions.py
+++ operations/functions.py
@@ -1,2 +1,7 @@
 def multiply_by_two(*digits):
     return list(map(lambda x: x * 2, digits))
+
+
+def sum_two_digit(a, b):
+    return a + b
+
