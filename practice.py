9c3769627d0c3cb7f5a9e78e28590157bc312cbc
is0ly <iljaosipov@gmail.com>
2021-01-07 14:44:17 +0300

    Practice

---
+++ practice.py
@@ -0,0 +1,18 @@
+import simple_draw as sd
+
+length = 200
+point_0 = sd.get_point(300, 300)
+
+
+def triangle(point, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=200, width=3)
+    v1.draw()
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=200, width=3)
+    v2.draw()
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle+240, length=200, width=3)
+    v3.draw()
+
+
+triangle(point=point_0)
+
+sd.pause()
